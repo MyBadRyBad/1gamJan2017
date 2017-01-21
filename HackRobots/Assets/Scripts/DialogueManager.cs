013102d16069b5d8d25217a05ae44b78899e7f42
Ryan B Badilla <ryan.badilla@gmail.com>
2017-01-21 15:59:40 -0800

    added dialogue manager

---
+++ HackRobots/Assets/Scripts/DialogueManager.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class DialogueManager : MonoBehaviour {
+
+	public GameObject dialogueBox;
+	public Text dialogueText;
+
+	public bool dialogueActive;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (dialogueActive && Input.GetKeyDown(KeyCode.Space)) {
+
+			// deactivate box
+			dialogueBox.SetActive (false);
+			dialogueActive = false;
+		}
+	}
+
+
+	// Show Dialogue Box on screen
+	public void ShowDialogueBox(string dialogue) {
+		dialogueActive = true;
+		dialogueBox.SetActive (true);
+		dialogueText.text = dialogue;
+	}
+}
---
+++ HackRobots/Assets/Scripts/PlayerCollision.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerCollision : MonoBehaviour {
+
+	void OnCollisionEnter2D(Collision2D coll) {
+		Debug.Log ("collision detected: Collision");
+
+		if (coll.gameObject.tag == "Robot") {
+			Debug.Log ("collided with robot.");
+		}
+	}
+}
