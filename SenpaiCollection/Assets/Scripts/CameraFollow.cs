0d0c62c556add1649e7a99e91d9b11cf013f8526
Ryan B Badilla <ryan.badilla@gmail.com>
2017-01-21 16:10:10 -0800

    added senpai collection project

---
+++ SenpaiCollection/Assets/Scripts/CameraFollow.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraFollow : MonoBehaviour {
+
+	public Transform target;
+	public float smoothing = 5.0f;
+
+	private Vector3 m_offset;
+
+	// Use this for initialization
+	void Start () {
+		m_offset = transform.position - target.position;
+	}
+
+	void FixedUpdate() {
+		Vector3 targetCamPos = target.position + m_offset;
+		transform.position = Vector3.Lerp (transform.position, targetCamPos, smoothing * Time.deltaTime);
+	}
+}
---
+++ SenpaiCollection/Assets/Scripts/PlayerController.cs
@@ -0,0 +1,71 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour {
+
+	public float speed = 6.0f;
+
+	private Vector3 m_movement;
+	private Animator m_anim;
+	private Rigidbody m_playerRigidbody;
+	private QuerySDMecanimController m_sdMecanimController;
+
+	void Awake() {
+		// Set up references.
+		m_anim = GetComponent <Animator> ();
+		m_playerRigidbody = GetComponent <Rigidbody> ();
+		m_sdMecanimController = GetComponent<QuerySDMecanimController> ();
+	}
+
+	// Use this for initialization
+	void Start () {
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+	void FixedUpdate ()
+	{
+		// Store the input axes.
+		float horizontal = Input.GetAxisRaw("Horizontal");
+		float vertical = Input.GetAxisRaw("Vertical");
+
+		// Move the player around the scene.
+		Move (horizontal, vertical);
+
+		// Animate the player.
+		Animating (horizontal, vertical);
+	}
+
+	void Move (float horizontal, float vertical)
+	{
+		// Set the movement vector based on the axis input.
+		m_movement.Set (horizontal, 0f, vertical);
+
+		// Normalise the movement vector and make it proportional to the speed per second.
+		m_movement = m_movement.normalized * speed * Time.deltaTime;
+
+		// Move the player to it's current position plus the movement.
+		m_playerRigidbody.MovePosition (transform.position + m_movement);
+
+		// rotate the player towards movement direction
+		if (horizontal != 0 || vertical != 0) {
+			transform.rotation = Quaternion.LookRotation (m_movement);
+		}
+	}
+
+	void Animating (float h, float v)
+	{
+		// Create a boolean that is true if either of the input axes is non-zero.
+		bool walking = (h != 0f) || (v != 0f);
+
+		if (walking) {
+			m_sdMecanimController.ChangeAnimation (QuerySDMecanimController.QueryChanSDAnimationType.NORMAL_WALK);
+		} else {
+			m_sdMecanimController.ChangeAnimation (QuerySDMecanimController.QueryChanSDAnimationType.NORMAL_STAND);
+		}
+	}
+}
